90e3324cddf416ef9cefda4af2043c5a133a7c6f
Kris <krzysztofkoz6@gmail.com>
2021-12-14 00:14:21 +0100

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+import "./style.css";
 import myName from "./myName";
+import Icon from "./icon.svg";
 
 function component() {
 	const element = document.createElement("div");
 
 	element.innerHTML = myName("Kris");
+	element.classList.add("hello");
+
+	const myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 }
