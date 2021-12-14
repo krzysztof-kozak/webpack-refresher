23746cb9cc46413e31d1accdb8391aa5361c568a
Kris <krzysztofkoz6@gmail.com>
2021-12-14 11:57:38 +0100

    Configure dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "development",
+
 	devtool: "inline-source-map",
+	devServer: {
+		host: "localhost",
+		port: 8080,
+		static: "./dist",
+	},
+
 	entry: {
 		index: "./src/index.js",
 		print: "./src/print.js",
 	},
+
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: "Developement",
 		}),
 	],
+
 	output: {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
+	},
+	optimization: {
+		runtimeChunk: "single",
 	},
 };
